7f8e1191bb84ae2c396c34ec9f2776fb12f51ce3
stolzz0613 <andres0613@utp.edu.co>
2020-07-22 01:11:17 -0500

    get api fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
 
@@ -6,18 +6,30 @@ function App() {
 
   const [categoria, setCategoria] = useState("")
 
-  return (
-    <Fragment>
-      <Header
-        titulo="Buscador de Noticias"
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=co&apiKey=ae3cf9d810c2449d8b6858cb05fe015b`;
+      const response = await fetch(url);
+      const noticias = await response.json();
+
+      console.log(noticias);
+    }
+    consultarAPI();
+  }
+}, [categoria])
+
+return (
+  <Fragment>
+    <Header
+      titulo="Buscador de Noticias"
+    />
+    <div className="container white">
+      <Formulario
+        setCategoria={setCategoria}
       />
-      <div className="container white">
-        <Formulario
-          setCategoria={setCategoria}
-        />
-      </div>
-    </Fragment>
-  );
+    </div>
+  </Fragment>
+);
 }
 
 export default App;

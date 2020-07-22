f5197282d4a273f9748366ee7f6ef59901eb2cd6
stolzz0613 <andres0613@utp.edu.co>
2020-07-21 23:50:14 -0500

    css Module

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+
+function App() {
+  return (
+    <Fragment>
+      <Header
+        titulo="Buscador de Noticias"
+      />
+      <div className="container white">
+        <Formulario>Formulario aquí</Formulario>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
